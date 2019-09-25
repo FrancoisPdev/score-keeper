103bb95ef988e2c4d450a71f7c80e3f506c250bd
François <parent.francois59@gmail.com>
2019-09-25 20:08:49 +0200

    restart script.js

--- js/script.js
+++ js/script.js
@@ -1,54 +1,56 @@
 var p1Button = document.querySelector("#p1");
-var p2Button = document.querySelector("#p2");
-var h1 = document.querySelector("h1");
-var p1Display = document.querySelector("#p1Score");
-var p2Display = document.querySelector("#p2Score");
+var p2Button =  document.querySelector("#p2");
 var resetButton = document.querySelector("#reset");
-var gameCounter = document.querySelector("input");
-var gameCount = document.querySelector("p span");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input[type='number']");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
-var maxScore = 5;
 var gameOver = false;
+var winningScore = 5;
 
-gameCounter.addEventListener('change', function () {
-  gameCount.textContext = this.value;
-  maxScore = Number(this.value);
-  reset();
-});
-
-p1Button.onclick = function () {
-  if(!gameOver) {
+p1Button.addEventListener("click", function(){
+  if (!gameOver) {
     p1Score++;
-    if (maxScore === p1Score) {
-     p1Display.classList.add("winner");
-     gameOver = !gameOver; 
+    if (p1Score===winningScore) {
+      gameOver=true;
+      p1Display.classList.add("winner");
     }
     p1Display.textContent = p1Score;
   }
-}
+});
 
-p2Button.onclick = function () {
-  if(!gameOver) {
+p2Button.addEventListener("click", function(){
+  if (!gameOver) {
     p2Score++;
-    if (maxScore === p2Score) {
-     p2Display.classList.add("winner");
-     gameOver = !gameOver; 
+    if (p2Score===winningScore) {
+      gameOver=true;
+      p2Display.classList.add("winner");
     }
     p2Display.textContent = p2Score;
   }
-}
+});
 
-resetButton.onclick = function () {
+resetButton.addEventListener("click", function(){
   reset();
+});
+
+function reset() {
+  p1Score = 0;//sets score to 0
+  p2Score = 0;
+
+  p1Display.textContent = p1Score;//sets text to zero
+  p2Display.textContent = p2Score;
+
+  p1Display.classList.remove("winner");//removes green color
+  p2Display.classList.remove("winner");
+
+  gameOver = false;
 }
 
-function reset () {
-  p1Score = 0;
-  p2Score = 0; 
-  p2Display.classList.remove("winner");
-  p1Display.classList.remove("winner");
-  p1Display.textContent = 0;
-  p2Display.textContent = 0;
-  gameOver = !gameOver;
-}
+numInput.addEventListener("change", function(){
+  winningScoreDisplay.textContent = this.value;
+  winningScore = Number(this.value);
+  reset();
+});
